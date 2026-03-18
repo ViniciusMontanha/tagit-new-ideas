e127c00b52832b304ad966fa69301142896b10aa
Vinicius Medeiros <148271805+ViniciusMontanha@users.noreply.github.com>
2026-03-18 13:22:58 -0300

    Update phone number in WhatsAppWidget

--- src/components/WhatsAppWidget.tsx
+++ src/components/WhatsAppWidget.tsx
@@ -6,7 +6,7 @@ import { MessageCircle } from "lucide-react";
  * Localização: Bottom-right, z-50
  */
 export const WhatsAppWidget = () => {
-  const phoneNumber = "5516996403745";
+  const phoneNumber = "5516992058150";
   const message = "Olá! Gostaria de mais informações sobre a Tag It.";
 
   const handleClick = () => {
